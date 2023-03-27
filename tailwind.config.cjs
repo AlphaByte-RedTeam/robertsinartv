0e941e88dcd82a409adb90dd648238373009b4d0
Andrew Virya Victorio <andrew.avv03@gmail.com>
2023-03-27 18:29:19 +0700

    chore: extend tailwind class

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,20 @@
 module.exports = {
 	content: ["./src/**/*.{html,js,svelte,ts}"],
 	theme: {
-		extend: {},
+		extend: {
+			dropShadow: {
+				'glassmorphism': '0 10px 20px rgba(0, 0, 0, 0.05)',
+			},
+			colors: {
+				glassmorphishm: {
+					8: 'rgba(32, 32, 32, 0.08)',
+					38: 'rgba(32, 32, 32, 0.38)',
+				},
+			},
+			fontFamily: {
+				'inter': ['Inter', 'sans-serif'],
+			},
+		},
 	},
 	plugins: [],
 }
